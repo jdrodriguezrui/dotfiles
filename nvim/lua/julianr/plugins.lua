a875474500057ae2e48e25b21839f883be2a0bb3
Julian R <julian@dotfiles>
2023-10-19 14:54:02 -0500

    Minor updates

--- nvim/lua/julianr/plugins.lua
+++ nvim/lua/julianr/plugins.lua
@@ -36,7 +36,7 @@ return require('packer').startup(function(use)
    }
    -- Telescope
    use {
-      'nvim-telescope/telescope.nvim', tag = '0.1.0',
+      'nvim-telescope/telescope.nvim', branch = '0.1.x',
       requires = { {'nvim-lua/plenary.nvim'} }
    }
    use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
